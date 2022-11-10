7e94b32a3fe0033ee24b901bd99225fc2d670b82
hieu <nguyendinhhieu9a1@gmail.com>
2022-11-10 16:35:47 +0700

    add animation

---
+++ Assets/Scripts/Towers/NavFollowEnemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Towers
+{
+    public class NavFollowEnemy : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        [SerializeField] private Transform player;
+
+        [SerializeField] private Rigidbody2D rb;
+        // Start is called before the first frame update
+
+        // Update is called once per frame
+        void Update()
+        {
+            var lookDir = (Vector2) player.position - rb.position;
+            var angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
+            rb.rotation = angle-180f;
+            lookDir.Normalize();
+        }
+    }
+}
